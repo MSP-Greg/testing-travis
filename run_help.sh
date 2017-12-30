ecef08ddf5b741cb6b57919824aefc7192d49386
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-12-30 12:32:15 -0600

    Install first 1.8.7 - 2.0.0 -v & --help

--- run_help.sh
+++ run_help.sh
@@ -1,13 +1,17 @@
 #! /bin/sh
 
+rvm install ruby-1.8.7 --binary > /dev/null 2>&1 && 
 rvm use 1.8.7 > /dev/null 2>&1 &&
-ruby --help
+ruby -v --help
 
+rvm install ruby-1.9.2-p330 --binary > /dev/null 2>&1 && 
 rvm use 1.9.2 > /dev/null 2>&1 &&
-ruby --help
+ruby -v --help
 
+rvm install ruby-1.9.3-p551 --binary > /dev/null 2>&1 && 
 rvm use 1.9.3 > /dev/null 2>&1 &&
-ruby --help
+ruby -v --help
 
-rvm use 2.0.0 > /dev/null 2>&1 &&
-ruby --help
+rvm install ruby-2.0.0-p648 --binary > /dev/null 2>&1 && 
+rvm use ruby-2.0.0-p648 &&
+ruby -v --help
