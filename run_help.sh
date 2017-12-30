0400a9c46896f30987bc6ded6c30b60185853d0f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2017-12-30 12:43:59 -0600

    Fixup 1.8.7?

--- run_help.sh
+++ run_help.sh
@@ -1,17 +1,21 @@
 #! /bin/sh
 
-rvm install ruby-1.8.7 --binary > /dev/null 2>&1 && 
+echo ———————————————————————————————————————————————————————————————————————————
+rvm install ruby-1.8.7-p371 --binary > /dev/null 2>&1 && 
 rvm use 1.8.7 > /dev/null 2>&1 &&
 ruby -v --help
 
+echo ———————————————————————————————————————————————————————————————————————————
 rvm install ruby-1.9.2-p330 --binary > /dev/null 2>&1 && 
 rvm use 1.9.2 > /dev/null 2>&1 &&
 ruby -v --help
 
+echo ———————————————————————————————————————————————————————————————————————————
 rvm install ruby-1.9.3-p551 --binary > /dev/null 2>&1 && 
 rvm use 1.9.3 > /dev/null 2>&1 &&
 ruby -v --help
 
+echo ———————————————————————————————————————————————————————————————————————————
 rvm install ruby-2.0.0-p648 --binary > /dev/null 2>&1 && 
 rvm use ruby-2.0.0-p648 &&
 ruby -v --help
