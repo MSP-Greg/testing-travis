6ba5583a07e7ada17373f0cb5faeedbbd024a60a
MSP-Greg <MSP-Greg@users.noreply.github.com>
2018-03-19 09:51:46 -0500

    Try xenial ?

--- run_help.sh
+++ run_help.sh
@@ -1,21 +1,11 @@
 #! /bin/sh
 
 echo ———————————————————————————————————————————————————————————————————————————
-rvm install ruby-1.8.7-p371 --binary > /dev/null 2>&1 && 
-rvm use 1.8.7 > /dev/null 2>&1 &&
-ruby -v --help
+rvm install 2.5.0 --binary > /dev/null 2>&1 &&
+rvm use 2.5.0 &&
+ruby travis_info.rb
 
 echo ———————————————————————————————————————————————————————————————————————————
-rvm install ruby-1.9.2-p330 --binary > /dev/null 2>&1 && 
-rvm use 1.9.2 > /dev/null 2>&1 &&
-ruby -v --help
-
-echo ———————————————————————————————————————————————————————————————————————————
-rvm install ruby-1.9.3-p551 --binary > /dev/null 2>&1 && 
-rvm use 1.9.3 > /dev/null 2>&1 &&
-ruby -v --help
-
-echo ———————————————————————————————————————————————————————————————————————————
-rvm install ruby-2.0.0-p648 --binary > /dev/null 2>&1 && 
-rvm use ruby-2.0.0-p648 &&
-ruby -v --help
+rvm install ruby-head --binary > /dev/null 2>&1 &&
+rvm use ruby-head &&
+ruby travis_info.rb
