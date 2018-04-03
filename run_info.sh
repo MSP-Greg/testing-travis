a930b17e5937ac4d9d2c18dc1f188258807f0725
MSP-Greg <MSP-Greg@users.noreply.github.com>
2018-04-02 20:58:35 -0500

    Encodings

--- run_info.sh
+++ run_info.sh
@@ -11,22 +11,26 @@ ruby travis_info.rb
 rvm use 2.2 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.2.8 --binary > /dev/null 2>&1 &&
-rvm use 2.2.8 &&
+rvm install 2.2.9 --binary > /dev/null 2>&1 &&
+rvm use 2.2.9 &&
 ruby travis_info.rb
 
 rvm use 2.3 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.3.5 --binary > /dev/null 2>&1 &&
-rvm use 2.3.5 &&
+rvm install 2.3.6 --binary > /dev/null 2>&1 &&
+rvm use 2.3.6 &&
 ruby travis_info.rb
 
 rvm use 2.4 > /dev/null 2>&1 &&
 ruby travis_info.rb
 
-rvm install 2.4.2 --binary > /dev/null 2>&1 &&
-rvm use 2.4.2 &&
+rvm install 2.4.4 --binary > /dev/null 2>&1 &&
+rvm use 2.4.4 &&
+ruby travis_info.rb
+
+rvm install 2.5.1 --binary > /dev/null 2>&1 &&
+rvm use 2.5.1 &&
 ruby travis_info.rb
 
 rvm install ruby-head --binary > /dev/null 2>&1 &&
