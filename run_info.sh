6f8f1b2f224be8c4598ae68cc5b8da1e46e2729f
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-03-09 10:35:24 -0600

    find & replace error...

--- run_info.sh
+++ run_info.sh
@@ -2,18 +2,18 @@
 
 rvm install ruby-2.0.0-p648 --binary > /dev/null 2>&1 && 
 rvm use ruby-2.0.0-p648 &&
-ruby ri2_142.rb
+ruby -rprofile ri2_142.rb
 
 rvm install 2.1.10 --binary > /dev/null 2>&1 &&
 rvm use 2.1.10 &&
-ruby ri2_142.rb
+ruby -rprofile ri2_142.rb
 
 rvm use 2.2 > /dev/null 2>&1 &&
-ruby ri2_142.rb
+ruby -rprofile ri2_142.rb
 
 rvm install 2.2.9 --binary > /dev/null 2>&1 &&
 rvm use 2.2.9 &&
-ruby ri2_142.rb
+ruby -rprofile ri2_142.rb
 
 rvm use 2.3 > /dev/null 2>&1 &&
 ruby -rprofile ri2_142.rb
