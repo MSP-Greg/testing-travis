7b9659e4feae1635accfc30251f39e9c45e9e0a6
MSP-Greg <MSP-Greg@users.noreply.github.com>
2019-03-28 14:22:07 -0500

    source location?

--- openssl_info.rb
+++ openssl_info.rb
@@ -4,9 +4,12 @@
 ruby C:\Greg\GitHub\testing-travis\openssl_info.rb
 =end
 
+gem 'openssl'
+
 require 'openssl'
 
 puts "", RUBY_DESCRIPTION
 puts "OpenSSL #{OpenSSL::VERSION}"
+puts "src #{OpenSSL::Buffering.instance_method(:close).source_location}"
 puts "Runtime #{OpenSSL::OPENSSL_LIBRARY_VERSION}"
 puts "Compile #{OpenSSL::OPENSSL_VERSION}", ""
